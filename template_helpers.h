1279ef3c09d28c94a89ae7ffc11a72ecfd06b053
Joe <joeacarstairs@gmail.com>
2021-04-18 18:53:05 +0100

    Add project files.

---
+++ template_helpers.h
@@ -0,0 +1,11 @@
+﻿// cpp-template-helpers.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+#include "is_comparison.h"
+#include "is_equatable.h"
+#include "is_member_of.h"
+#include "is_predicate.h"
---
+++ tester.cpp
@@ -0,0 +1,12 @@
+﻿// cpp-template-helpers.cpp : Defines the entry point for the application.
+//
+
+#include "template_helpers.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
