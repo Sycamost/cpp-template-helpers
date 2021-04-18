64666e1204eb77cfea6e3ccea9cefe3f907df901
Joe <joeacarstairs@gmail.com>
2021-04-18 21:29:46 +0100

    Adds convert_string.

---
+++ convert_string.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <type_traits>
+#include <string>
+#include <sstream>
+#include "is_streamable.h"
+
+template<class T, std::enable_if_t<std::is_convertible_v<T, std::string>, bool> = true>
+std::string convert_string(T t) {
+    return static_cast<std::string>(t);
+}
+
+template <
+    class T,
+    std::enable_if_t<
+        std::conjunction_v<
+            std::negation<std::is_convertible<T, std::string>>,
+            is_streamable<T>
+        >,
+        bool
+    > = true
+>
+std::string convert_string(T t) {
+    std::ostringstream stream = std::ostringstream();
+    stream << t;
+    return stream.str();
+}
+
+template<class T>
+using can_convert_string = std::disjunction<std::is_convertible<T, std::string>, is_streamable<T>>;
+
+template<class T>
+inline constexpr bool can_convert_string_v = can_convert_string<T>::value;
