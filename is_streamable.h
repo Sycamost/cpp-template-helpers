03a3c4c0368d2f13bb412e06271fed889204a2f9
Joe <joeacarstairs@gmail.com>
2021-04-20 16:03:08 +0100

    Tidies up template parameters.

--- is_streamable.h
+++ is_streamable.h
@@ -12,7 +12,7 @@ namespace template_helpers
     template<
         typename _Type,
         typename _Stream = std::ostringstream,
-        typename _StreamElem = std::ostringstream::char_type,
+        typename _StreamElem = _Stream::char_type,
         typename _StreamTraits = std::char_traits<_StreamElem>,
         std::enable_if_t<std::is_base_of_v<std::basic_ostringstream<_StreamElem, _StreamTraits>, _Stream>, bool> = true
     >
@@ -29,15 +29,21 @@ namespace template_helpers
         static const bool value = decltype(test<_Type, _Stream>(0))::value;
     };
 
-    template<typename _Type, typename _Stream = std::ostringstream>
-    inline constexpr bool is_streamable_v = is_streamable<_Type, _Stream>::value;
+    template<
+        typename _Type,
+        typename _Stream = std::ostringstream,
+        typename _StreamElem = _Stream::char_type,
+        typename _StreamTraits = std::char_traits<_StreamElem>,
+        std::enable_if_t<std::is_base_of_v<std::basic_ostringstream<_StreamElem, _StreamTraits>, _Stream>, bool> = true
+    >
+    inline constexpr bool is_streamable_v = is_streamable<_Type, _Stream, _StreamElem, _StreamTraits>::value;
 
     template<
         typename _Type,
         typename _Stream = std::wostringstream,
         std::enable_if_t<std::is_base_of_v<std::basic_ostringstream<wchar_t, std::char_traits<wchar_t>>, _Stream>, bool> = true
     >
-    using is_w_streamable = is_streamable<_Type, _Stream, wchar_t, std::char_traits<wchar_t>>;
+    using is_w_streamable = is_streamable<_Type, _Stream>;
 
     template<typename _Type, typename _Stream = std::wostringstream>
     inline constexpr bool is_w_streamable_v = is_w_streamable<_Type, _Stream>::value;
