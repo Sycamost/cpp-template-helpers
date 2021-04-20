4cf5c7c003ec7aa79683dba4f21dd614a5803cd4
Joe <joeacarstairs@gmail.com>
2021-04-20 11:58:05 +0100

    Implements is_w_streamable to check if a given type can be written to a given stream of wide chars. Also, prefers ostream to ostringstream (and wostream to wostringstream).

--- tester.cpp
+++ tester.cpp
@@ -3,13 +3,15 @@
 
 #include "template_helpers.h"
 
+using namespace template_helpers;
+
 int main()
 {
-	static_assert(is_streamable_v<const int&>, "Error!");
-	static_assert(is_streamable_v<const float&>, "Error!");
-	static_assert(is_streamable_v<const int*&>, "Error!");
-	static_assert(is_streamable_v<const int[]>, "Error!");
-	static_assert(is_streamable_v<const void*>, "Error!");
-	static_assert(is_streamable_v<const void(*)()>, "Error!");
+	static_assert(is_w_streamable_v<const int&>, "Error!");
+	static_assert(is_w_streamable_v<const float&>, "Error!");
+	static_assert(is_w_streamable_v<const int*&>, "Error!");
+	static_assert(is_w_streamable_v<const int[]>, "Error!");
+	static_assert(is_w_streamable_v<const void*>, "Error!");
+	static_assert(is_w_streamable_v<const void(*)()>, "Error!");
 	return 0;
 }
